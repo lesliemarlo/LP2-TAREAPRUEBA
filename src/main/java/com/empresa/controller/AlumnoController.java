ad760a17cec8153d2c4b7707df063aa106cf912d
lesliemarlo <lesliemarlo09@gmail.com>
2023-09-02 12:48:20 -0500

    Tarea Leslie

--- src/main/java/com/empresa/controller/AlumnoController.java
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -1,10 +1,21 @@
 package com.empresa.controller;
 
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.empresa.entity.Alumno;
+import com.empresa.service.AlumnoService;
 
 @Controller
 public class AlumnoController {
+	
+	@Autowired
+	private AlumnoService service;
 
 	
 	@GetMapping(value = "/verAlumno" )
@@ -12,5 +23,21 @@ public class AlumnoController {
 		return "registraAlumno";
 	}
 	
-
+	@PostMapping("/registraAlumno")
+	@ResponseBody
+	public HashMap<?, ?> inserta(Alumno obj)
+	{
+		HashMap<String, String> salida = new HashMap<>()	;
+		
+		Alumno objSalida = service.insertaAlumno(obj);
+		if(objSalida == null) {
+			salida.put("MENSAJE", "Error en el registro");
+			
+		}else {
+			salida.put("MENSAJE", "Registro exitoso");
+		}
+		return  salida;
+		
+	
+				}
 }
